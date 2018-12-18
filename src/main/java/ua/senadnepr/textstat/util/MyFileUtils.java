056586a60da0bc8ead91fa638b83f9218a1b6e1b
senadnepr <nexans@ua.fm>
2018-12-18 12:01:45 +0200

    fix minWord=0 part2

--- src/main/java/ua/senadnepr/textstat/util/MyFileUtils.java
+++ src/main/java/ua/senadnepr/textstat/util/MyFileUtils.java
@@ -1,4 +1,5 @@
 package ua.senadnepr.textstat.util;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ua.senadnepr.textstat.model.TextFile;
@@ -9,7 +10,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 public class MyFileUtils {
@@ -33,9 +38,8 @@ public class MyFileUtils {
             if ("Empty file error".equals(e.getMessage())) {
                 try {
                     Files.delete(target.toPath());
-                    System.out.println(target.toPath().toString());
                 } catch (IOException e1) {
-                    e1.printStackTrace();
+                    throw new ApplicationException("Error delete empty file");
                 }
             }
             throw e;
@@ -80,34 +84,35 @@ public class MyFileUtils {
 
         String name = fileString.name.getFileName().toString();
         String fullName = fileString.name.toString();
-        Integer numberOfString = fileString.strings.size();
-        if(fileString.strings.get(0) == null) throw new ApplicationException("File is empty");
-        int nword = 0;
-        Integer maxWord = 0;
-        Integer minWord = 0;
+        Integer numberOfString = 0;
+        if(fileString.strings.get(0) == null) throw new ApplicationException("Empty file error");
+        int numberOfWordInFile = 0;
+        List<Integer> maxWordNumberArray = new ArrayList<>();
+        List<Integer> minWordNumberArray = new ArrayList<>();
+
         for (String s: fileString.strings
              ) {
-
+            if("".equals(s)) continue;
+            numberOfString++;
             String delims = "[ .,?!]+";
-            String[] words = s.split(delims);
-            nword += words.length;
 
-            Integer max = null;
-            Integer min = null;
+            List<String> wordsInString = Arrays.stream(s.split(delims)).filter(e-> e.length()>0).collect(Collectors.toList());
+
+            numberOfWordInFile += wordsInString.size();
+
             try {
-                max = Arrays.stream(words).mapToInt(String::length).max().getAsInt();
-                min = Arrays.stream(words).mapToInt(String::length).min().getAsInt();
+                maxWordNumberArray.add(wordsInString.stream().mapToInt(String::length).max().getAsInt());
+                minWordNumberArray.add(wordsInString.stream().mapToInt(String::length).min().getAsInt());
             } catch (Exception e) {
                 throw new ApplicationException("Empty file error");
             }
 
-            maxWord = (maxWord < max) ? max : maxWord;
-            minWord = maxWord;
-            minWord = (minWord > min) ? min : minWord;
-
         }
+        if(numberOfWordInFile == 0 || numberOfString == 0) throw new ApplicationException("Empty file error");
 
-        Integer averageNumber = nword / fileString.strings.size();
+        Integer averageNumber = numberOfWordInFile / numberOfString;
+        Integer maxWord = Collections.max(maxWordNumberArray);
+        Integer minWord = Collections.min(minWordNumberArray);
 
         return new TextFile(null, name, fullName, numberOfString, averageNumber, maxWord, minWord);
     }
